4d1f7d383d54ed346b775695eaf26263927cd7f9
franz <mattefranzz@gmail.com>
2024-05-06 11:58:05 +0200

    neo tree toggle off

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -9,11 +9,11 @@ return {
 	config = function()
 		vim.keymap.set("n", "<C-n>", ":Neotree filesystem toggle left<CR>", {})
 		vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
-    vim.api.nvim_create_autocmd("VimEnter", {
-      command = "set nornu nonu | Neotree toggle",
-    })
-    vim.api.nvim_create_autocmd("BufEnter", {
-      command = "set rnu nu",
-    })
+    -- vim.api.nvim_create_autocmd("VimEnter", {
+    --   command = "set nornu nonu | Neotree toggle",
+    -- })
+    -- vim.api.nvim_create_autocmd("BufEnter", {
+    --  command = "set rnu nu",
+    -- })
 	end,
 }
